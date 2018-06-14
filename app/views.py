04b816b3992ced5486a5556acda502cf003cd0cb
Leo Igane <iganeleo@gmail.com>
2018-06-14 19:13:15 +0300

    renders the news page title

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from flask import render_template
+from app import app
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+    '''
+    Views news page function that returns the news articles page and its data
+    '''
+    return render_template('news.html',id = news_id, title = id 
+
+#views
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = 'Home - Welcome to The best Online News Platform'
+    return render_template('index.html',title = title)
