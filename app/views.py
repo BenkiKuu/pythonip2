d9387084174201b3b5079bcd7cb0383e586d0a7b
Leo Igane <iganeleo@gmail.com>
2018-06-18 21:58:59 +0300

    Deals with the block elements bug

--- app/views.py
+++ app/views.py
@@ -1,12 +1,7 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
-@app.route('/news/<int:news_id>')
-def news(news_id):
-    '''
-    Views news page function that returns the news articles page and its data
-    '''
-    return render_template('news.html',id = news_id, title = id )
 
 #views
 @app.route('/')
@@ -14,5 +9,18 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    # Getting popular News
+    top_news_soueces = get_news('sources')
+    print(top_news_sources)
     title = 'Home - Welcome to The best Online News Platform'
-    return render_template('index.html',title = title)
+    return render_template('index.html',title = title, headlines = top_news_sources)
+
+
+
+
+@app.route('/news/<int:news_id>')
+def news(news_id):
+    '''
+    Views news page function that returns the news articles page and its data
+    '''
+    return render_template('news.html',id = news_id, title = id )
